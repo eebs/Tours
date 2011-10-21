fba3a789920d95335004021d27cb9d0ff460cd67
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-10-21 13:39:37 -0400

    added model to controller for use in all actions

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -5,12 +5,14 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
-        /* Initialize action controller here */
+        // Get authentication model
+		$this->_authModel = new Application_Model_Authentication();
     }
 
     public function indexAction()
     {
-        // action body
+        // Get client
+		$client = $this->_authModel->getClientByPublicKey('JMoEudX2ESn5ZNiUcMbFd25ynBErffCF7l4ezRWRe959PENv6XVYNckiImF7P34Q');
     }
 
 

061029b46b07a8ad3a1aa7e4d28550f8ab26f756
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-10-21 16:38:11 -0400

    removed unneeded logging from RestAuth

--- library/Tours/Controller/Plugin/RestAuth.php
+++ library/Tours/Controller/Plugin/RestAuth.php
@@ -9,15 +9,12 @@ class Tours_Controller_Plugin_RestAuth extends Zend_Controller_Plugin_Abstract
 			$dateHeader = $request->getHeader('Date');
 
 			// maybe check date to see if it's within 180 seconds of current time?
-			$logger = Zend_Registry::get('log');
+			$isAuthed = false;
 
 			if ($authHeader && $dateHeader) {
 				$authModel = new Application_Model_Authentication();
 				$method = ucfirst(strtolower($request->getMethod()));
 				$isAuthed = $authModel->authenticate($authHeader, $dateHeader, $method);
-				$logger->debug($isAuthed);
-			}else{
-				$isAuthed = false;
 			}
 
 			if(!$isAuthed){

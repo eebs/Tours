0db1d205500182848265c0a399c4ec2113ebf861
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-10-21 13:01:18 -0400

    added default actions to ApiController, renamed

---
+++ application/controllers/ApiController.php
@@ -0,0 +1,41 @@
+<?php
+
+class ApiController extends Zend_Rest_Controller
+{
+
+	public function init()
+	{
+        $this->_helper->viewRenderer->setNoRender(true);
+	}
+
+	public function indexAction()
+	{
+		$this->getResponse()
+			->appendBody("From indexAction() returning all tours\n");
+	}
+
+	public function getAction()
+	{
+		$this->getResponse()
+			->appendBody("From getAction() returning the requested tour\n");
+	}
+
+	public function postAction()
+	{
+		$this->getResponse()
+			->appendBody("From postAction() creating the requested tour\n");
+	}
+
+	public function putAction()
+	{
+		$this->getResponse()
+			->appendBody("From putAction() updating the requested tour\n");
+
+	}
+
+	public function deleteAction()
+	{
+		$this->getResponse()
+			->appendBody("From deleteAction() deleting the requested tour\n");
+	}
+}

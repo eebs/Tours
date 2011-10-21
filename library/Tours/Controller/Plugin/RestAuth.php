44684fd7cb02e87c1c9652bfad7e1a146e7ed194
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-10-21 13:00:34 -0400

    only check for auth if requesting /api

--- library/Tours/Controller/Plugin/RestAuth.php
+++ library/Tours/Controller/Plugin/RestAuth.php
@@ -3,17 +3,20 @@ class Tours_Controller_Plugin_RestAuth extends Zend_Controller_Plugin_Abstract
 {
     public function preDispatch(Zend_Controller_Request_Abstract $request)
     {
-        $apiKey = $request->getHeader('Authorization');
+		// If the request is going to the API, check for authorization
+        if($request->getControllerName() == "api"){
+			$apiKey = $request->getHeader('Authorization');
 
-        if ($apiKey != 'secret') {
-            $this->getResponse()
-                    ->setHttpResponseCode(403)
-                    ->appendBody("Invalid API Key\n")
-                    ;
-            $request->setModuleName('default')
-                        ->setControllerName('error')
-                        ->setActionName('access')
-                        ->setDispatched(true);
-        }
+			if ($apiKey != 'secret') {
+				$this->getResponse()
+						->setHttpResponseCode(403)
+						->appendBody("Invalid API Key\n")
+						;
+				$request->setModuleName('default')
+							->setControllerName('error')
+							->setActionName('access')
+							->setDispatched(true);
+			}
+		}
     }
 }

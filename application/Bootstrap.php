8567a3aee9658288792bad57e36cdaa6658df294
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-10-21 11:11:40 -0400

    Initialize rest route in bootstrap.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,37 @@
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
 
+	/**
+     * @var Zend_Log
+     */
+    protected $_logger;
+
+    /**
+     * Setup the logging
+     */
+    protected function _initLogging()
+    {
+        $this->bootstrap('frontController');
+        $logger = new Zend_Log();
+
+        $writer = new Zend_Log_Writer_Stream(APPLICATION_PATH . '/../data/logs/app.log');
+        $logger->addWriter($writer);
+
+		if ('production' == $this->getEnvironment()) {
+			$filter = new Zend_Log_Filter_Priority(Zend_Log::CRIT);
+			$logger->addFilter($filter);
+		}
+
+        $this->_logger = $logger;
+        Zend_Registry::set('log', $logger);
+    }
+	
+	protected function _initRestRoute()
+	{
+        $this->bootstrap('frontController');
+        $frontController = Zend_Controller_Front::getInstance();
+        $restRoute = new Zend_Rest_Route($frontController);
+        $frontController->getRouter()->addRoute('default', $restRoute);
+	}
 
 }
-

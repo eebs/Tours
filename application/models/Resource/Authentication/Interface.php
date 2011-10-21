f9416c10549f7428961edf5b5e04ae4cac7a8abf
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-10-21 13:38:59 -0400

    fixed a typo

--- application/models/Resource/Authentication/Interface.php
+++ application/models/Resource/Authentication/Interface.php
@@ -1,6 +1,6 @@
 <?php
 
-interface Application_Model_Resource_Authentiction_Interface extends Dm_Model_Resource_Db_Interface 
+interface Application_Model_Resource_Authentication_Interface extends Dm_Model_Resource_Db_Interface 
 {
     public function getClientByPublicKey($publicKey);
 }

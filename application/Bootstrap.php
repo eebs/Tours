e33cb001ce979ee702a55a44edf34c2892bffab1
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-10-21 19:14:45 -0400

    Added user agent to the log line.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -18,11 +18,12 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 
         $writer = new Zend_Log_Writer_Stream(APPLICATION_PATH . '/../data/logs/app.log');
 		/* Use a customized Formatting and add custom log Events */
-		$formatter = new Zend_Log_Formatter_Simple('[%timestamp%] [%priorityName%] [%ipvisitor%] %requestmethod%: %uri% - %message%' . PHP_EOL);
+		$formatter = new Zend_Log_Formatter_Simple('[%timestamp%] [%priorityName%] [%ipvisitor%] %requestmethod%: %uri% - %useragent% - %message%' . PHP_EOL);
 		$writer->setFormatter($formatter);
 		$logger->setEventItem('requestmethod', $_SERVER['REQUEST_METHOD']);
 		$logger->setEventItem('uri', $_SERVER['REQUEST_URI']);
 		$logger->setEventItem('ipvisitor', $_SERVER['REMOTE_ADDR']);
+		$logger->setEventItem('useragent', $_SERVER['HTTP_USER_AGENT']);
 		$logger->setEventItem('timestamp', date('m-d-Y H:i:s', time()));
 		
         $logger->addWriter($writer);

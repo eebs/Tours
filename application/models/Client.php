482890e3efaa094944b8e2d8e8def68423556c65
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-10-24 04:32:26 -0400

    added methods to create and update clients

--- application/models/Client.php
+++ application/models/Client.php
@@ -27,4 +27,59 @@ class Application_Model_Client extends Dm_Model_Abstract
 			throw new Tours_Exception_Authentication_ClientNotAuthorized("Client is not authorized to perform that action.");
 		}
 	}
+
+    public function createClient($info = array())
+    {
+        $keypair = $this->generateKeypair();
+        $data['publicKey'] = $keypair['public'];
+        $data['privateKey'] = $keypair['private'];
+        foreach($info as $col => $value){
+            $data[$col] = $value;
+        }
+
+        return $this->save($data, array('email' => 'eebs@eebs-tech.com'));
+    }
+
+    public function updateClient($post)
+    {
+        return $this->save($post, array('email' => 'eebs@eebs-tech.com'));
+    }
+
+    protected function save($data, $defaults=array())
+    {
+        // apply any defaults
+        foreach ($defaults as $col => $value) {
+            $data[$col] = $value;
+        }
+
+        $user = $this->getResource('Client')->getClientByPublicKey($data['publicKey']);
+        return $this->getResource('Client')->saveRow($data, $user);
+    }
+
+    public function generateKeypair(){
+        $keypair = array(
+            'public'    => '',
+            'private'   => '',
+        );
+
+        $seeds = 'abcdefghijklmnopqrstuvwqyzABCDEFGHIJKLMNOPQRSTUVWQYZ0123456789';
+        $length = 64;
+
+        // Seed generator
+        list($usec, $sec) = explode(' ', microtime());
+        $seed = (float) $sec + ((float) $usec * 100000);
+        mt_srand($seed);
+
+        $seedings = 'abcdefghijklmnopqrstuvwqyzABCDEFGHIJKLMNOPQRSTUVWQYZ0123456789';
+
+        // Generate
+        $seeds_count = strlen($seeds);
+        for ($i = 0; $length > $i; $i++){
+            $keypair['public'] .= $seeds{mt_rand(0, $seeds_count - 1)};
+        }
+        for ($i = 0; $length > $i; $i++){
+            $keypair['private'] .= $seeds{mt_rand(0, $seeds_count - 1)};
+        }
+        return $keypair;
+    }
 }

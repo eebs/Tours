507fb627c1a1bc154c983dcdf75bbe7e1ef8555c
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-10-24 03:36:00 -0400

    set the default accept type

--- library/Tours/Controller/Plugin/AcceptHandler.php
+++ library/Tours/Controller/Plugin/AcceptHandler.php
@@ -2,19 +2,20 @@
 
 class Tours_Controller_Plugin_AcceptHandler extends Zend_Controller_Plugin_Abstract
 {
-	public function dispatchLoopStartup(Zend_Controller_Request_Abstract $request)
-	{
-		$this->getResponse()->setHeader('Vary', 'Accept');
-		$header = $request->getHeader('Accept');
-		switch (true){
-			case (FALSE !== strpos($header, 'application/json')):
-				$request->setParam('format', 'json');
-				break;
-			case ((FALSE !== strpos($header, 'application/xml'))):
-				$request->setParam('format', 'xml');
-				break;
-			default:
-				break;
-		}
-	}
+    public function dispatchLoopStartup(Zend_Controller_Request_Abstract $request)
+    {
+        $this->getResponse()->setHeader('Vary', 'Accept');
+        $header = $request->getHeader('Accept');
+        switch (true){
+            case (FALSE !== strpos($header, 'application/json')):
+                $request->setParam('format', 'json');
+                break;
+            case ((FALSE !== strpos($header, 'application/xml'))):
+                $request->setParam('format', 'xml');
+                break;
+            default:
+                $request->setParam('format', 'json');
+                break;
+        }
+    }
 }

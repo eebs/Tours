825134133e023fdba08b77a58ae6261a0b289ddc
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-11-03 14:26:52 -0400

    added method to get media from tours

--- application/models/Resource/Tour/Item.php
+++ application/models/Resource/Tour/Item.php
@@ -1,4 +1,19 @@
 <?php
 class Application_Model_Resource_Tour_Item extends Dm_Model_Resource_Db_Table_Row_Abstract implements Application_Model_Resource_Tour_Item_Interface
 {
+
+    /**
+     * Get media for tour
+     *
+     * @return Zend_Db_Table_Rowset containing Application_Model_Resource_Media_Item
+     */
+    public function getMedia()
+    {
+        $select = $this->select();
+		$select->from(array('m' => 'media'), array('uniqueFileName'));
+        return $this->findDependentRowset('Application_Model_Resource_Media',
+            'Media',
+            $select
+        );
+    }
 }

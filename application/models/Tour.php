16ba29d5c22aa202342635f1670b58bbf22e2abb
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-11-09 16:44:27 -0500

    added create/edit tour methods that use save()

--- application/models/Tour.php
+++ application/models/Tour.php
@@ -10,4 +10,31 @@ class Application_Model_Tour extends Dm_Model_Abstract
     {
         return $this->getResource('Tour')->getTours($paged, $order);
     }
+
+    public function createTour($post)
+    {
+        $form = $this->getForm('tourCreate');
+        return $this->save($form, $post);
+    }
+
+    public function editTour($post){
+        $form = $this->getForm('tourEdit');
+        return $this->save($form, $post);
+    }
+
+    protected function save($form, $post, $defaults = array()){
+        if(!$form->isValid($post)){
+            return false;
+        }
+
+        $filteredData = $form->getValues();
+
+        $data = $defaults;
+        foreach($filteredData as $col => $value){
+            $data[$col] = $value;
+        }
+
+        $tour = array_key_exists('id', $data) ? $this->getResource('Tour')->getTourById($data['id']) : null;
+        return $this->getResource('Tour')->saveRow($data, $tour);
+    }
 }

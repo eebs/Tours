e181e44c80634ccbb8ad2bf956ed1f662ebe8e41
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-11-16 14:22:29 -0500

    added tour as the only controller in the Rest route

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -41,7 +41,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 	{
         $this->bootstrap('frontController');
         $frontController = Zend_Controller_Front::getInstance();
-        $restRoute = new Zend_Rest_Route($frontController, array(), array('api'));
+        $restRoute = new Zend_Rest_Route($frontController, array(), array('api' => array('tour')));
         $frontController->getRouter()->addRoute('api', $restRoute);
 	}
     

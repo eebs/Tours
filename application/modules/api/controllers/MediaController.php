fee41990fb1e213347be13e3969fd911d86cccff
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-11-17 12:54:57 -0500

    Added error checking for tour and stop param

--- application/modules/api/controllers/MediaController.php
+++ application/modules/api/controllers/MediaController.php
@@ -4,7 +4,6 @@ class Api_MediaController extends Zend_Controller_Action
 {
     public function init()
     {
-        //$this->_helper->viewRenderer->setNoRender(true);
         $contextSwitch = $this->_helper->getHelper('contextSwitch');
         $contextSwitch
             ->addActionContext('upload', array('json','xml'))
@@ -12,6 +11,13 @@ class Api_MediaController extends Zend_Controller_Action
     }
 
     public function uploadAction(){
+        $tour = $this->getRequest()->getParam('tour', false);
+        $stop = $this->getRequest()->getParam('stop', false);
+        if(!($tour && $stop)){
+            $format = $this->getRequest()->getParam('format');
+            Zend_Registry::set('error_message', "Tour ID and Stop ID must be included in request");
+            return $this->_forward('error', 'error', 'api', array('format'=>$format));
+        }
         $upload = new Zend_File_Transfer();
         $upload->addValidators(array(
             'Size'      => array(

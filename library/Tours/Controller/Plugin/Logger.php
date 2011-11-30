7d7134bac48ef7e61de94cce742f37daf0d05f0b
Ebrahim Kobeissi <eebs@eebs-tech.com>
2011-11-30 17:06:36 -0500

    added header debugging to logger

--- library/Tours/Controller/Plugin/Logger.php
+++ library/Tours/Controller/Plugin/Logger.php
@@ -4,6 +4,12 @@ class Tours_Controller_Plugin_Logger extends Zend_Controller_Plugin_Abstract
     public function preDispatch(Zend_Controller_Request_Abstract $request)
     {
 		$logger = Zend_Registry::get('log');
+
+		$headers = array();
+		if (function_exists('apache_request_headers')) {
+			$headers = apache_request_headers();
+		}
 		$logger->info('Incoming Request');
+		$logger->debug(print_r($headers,true));
     }
 }
